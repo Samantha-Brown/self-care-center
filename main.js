f890405511ad737583cc384f9c8a39dab591fac2
Samantha Brown <bro7445@gmail.com>
2021-05-30 14:35:46 -0400

    Refactor some JS & CSS bugs

--- main.js
+++ main.js
@@ -4,49 +4,36 @@ var affirmations = ["I forgive myself and set myself free.", "I believe I can be
 var mantras = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.", "Don’t let yesterday take up too much of today.", "Every day is a second chance.", "Tell the truth and love everyone.", "I am free from sadness.", "I am enough.", "In the beginning it is you, in the middle it is you and in the end it is you.", "I love myself.", "I am present now.", "Inhale the future, exhale the past.", "This too shall pass.", "Yesterday is not today.", "The only constant is change.", "Onward and upward.", "I am the sky, the rest is weather."]
 
 
-var chooseAffirmations = document.getElementById("affirmation-selection")
-var chooseMantras = document.getElementById("mantra")
-var imgHolder = document.querySelector(".img-holder");
-var mainPage = document.querySelector(".the-whole-page")
-var newMessageButton = document.querySelector(".message-display");
-var randomMessages = document.querySelector(".receive-messages")
-
-
-window.addEventListener("load", hideSaying);
+window.addEventListener("load", hideMessage);
 var receiveMessageButton = document.querySelector('.receive-message');
-var mantraPhrase = document.querySelector('.mantra-phrase');
-var checkAffirmationRadio = document.getElementById('affirmation-radio');
-var checkMantraRadio = document.getElementById('mantra-radio');
-var meditationImage = document.querySelector('.meditation-img');
+var mantraPhrase = document.querySelector('.mantra-selection');
+var selectAffirmationRadio = document.getElementById('affirmation-message');
+var selectMantraRadio = document.getElementById('mantra-message');
+var meditationImage = document.querySelector('.meditation-person-img');
 
 
-receiveMessageButton.addEventListener('click', createRandomMessage);
+receiveMessageButton.addEventListener('click', viewRandomMessage);
 backToMainPageButton.addEventListener('click', showMainPage);
-viewFavoritesPage.addEventListener('click', deleteMessage);
+
+function getRandomIndex(array)
+
+  return Math.floor(Math.random() * array.length);
+}
 
 function hideImage() {
   meditationImage.classList.add('hidden');
 }
 
-function showFavoriteButton() {
-  event.preventDefault();
-  favoriteButton.classList.remove('hidden');
-  secondFavoriteButton.classList.remove('hidden');
-}
-
 function hideMainPage() {
   mainPage.classList.add('hidden');
   viewFavoritesPage.classList.remove('hidden');
-
 }
 
 function showMainPage() {
   mainPage.classList.remove('hidden');
   viewFavoritesPage.classList.add('hidden');
 }
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
+
 function selectAffirmation() {
    var randomAffirmation = affirmations[getRandomIndex(affirmations)];
    currentQuote = randomAffirmation;
@@ -56,15 +43,14 @@ function selectAffirmation() {
    var randomMantra = mantras[getRandomIndex(mantras)];
    currentQuote = randomMantra;
  }
- function createRandomMessage() {
-   if (checkAffirmationRadio.checked) {
+
+ function viewRandomMessage() {
+   if (selectAffirmationRadio.checked) {
     hideImage();
     selectAffirmation();
-    quoteArea.innerText = currentQuote;
+    quoteSection.innerText = currentQuote;
     showFavoriteButton();
-  } else if (checkMantraRadio.checked) {
+  } else if (selectMantraRadio.checked) {
     hideImage();
     selectMantra();
-    quoteArea.innerText = currentQuote;
-
-    showFavoriteButton();
+    quoteSection.innerText = currentQuote;
